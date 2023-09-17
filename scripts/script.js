e0482392816e0b80cfbaa2b375c83108e38523a8
WildG <wildgoncher@gmail.com>
2023-09-17 13:07:32 +0300

    Beta ver.1.1.0.

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,19 @@
-document.addEventListener('DOMContentLoaded', nav)
-function nav(){
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.main-nav');
-    burger.addEventListener('click', ()=>{
-        nav.classList.toggle('show')
-    })
+document.addEventListener("DOMContentLoaded", nav);
+function nav() {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".main-nav");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
 }
 
-
-document.addEventListener('DOMContentLoaded', phone)
+document.addEventListener("DOMContentLoaded", phone);
 function phone() {
-    const phoneBtn = document.getElementById('header-btn');
-    phoneBtn.onmouseover = ()=> {
-        phoneBtn.innerHTML = "8-800-555-55-55";
-    }
-    phoneBtn.onmouseout = ()=> {
-        phoneBtn.innerHTML = "Позвонить";
-    }
+  const phoneBtn = document.getElementById("header-btn");
+  phoneBtn.onmouseover = () => {
+    phoneBtn.innerHTML = "8-800-555-55-55";
+  };
+  phoneBtn.onmouseout = () => {
+    phoneBtn.innerHTML = "Позвонить";
+  };
 }
-
